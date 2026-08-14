86fc1b3bed4ded4ef70f72eebaaf02f89bb94fbd
nathan270109 <natiel228@gmail.com>
2026-08-14 16:48:43 -0300

    atividade

--- atividade/src/app/home/atividades/atividades.ts
+++ atividade/src/app/home/atividades/atividades.ts
@@ -4,11 +4,12 @@ import { Contador } from './contador/contador';
 import { ImagemIlustrativa } from './imagem-ilustrativa/imagem-ilustrativa';
 import { ArrayUsuarios } from './array-usuarios/array-usuarios';
 import { Formulario } from './formulario/formulario';
+import { Notas } from "./notas/notas";
 
 
 @Component({
   selector: 'app-atividades',
-  imports: [Cadastro, Contador, ImagemIlustrativa, ArrayUsuarios, Formulario],
+  imports: [Cadastro, Contador, ImagemIlustrativa, ArrayUsuarios, Formulario, Notas],
   templateUrl: './atividades.html',
   styleUrl: './atividades.css',
 })
